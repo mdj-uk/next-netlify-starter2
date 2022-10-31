03443a8fb72d0a39056664fe0896b84c5fe49132
jay <michaeldjay@gmail.com>
2022-10-31 23:40:24 +0000

    tidy up plugin

--- plugins/cache-images/index.js
+++ plugins/cache-images/index.js
@@ -1,41 +1,43 @@
 import fs from 'fs'
 import path from 'path'
-import Path from 'path'
+
 
 export const onPreBuild = async function ({ utils }) {
+   
    let imageManifestFile='./public/images/next-image-export-optimizer-hashes.json';
-   // Returns false if the file/directory was not cached yet. Returns true otherwise.
+   let sizes = [10, 32, 1000];
+   
    let imageManifestRestored = await utils.cache.restore(imageManifestFile)
+   
    if (imageManifestRestored){
-      let sizes = [10, 32, 1000];
-      
       console.log('Found image manifest. Will attempt to parse ... ');
       
       const fileContents = await fs.promises.readFile(imageManifestFile);
       const data = JSON.parse(fileContents);
-      // console.log(data);
 
       let preOptimisedImageFiles = Object.keys(data);
       for (let srcFile of preOptimisedImageFiles){
+        
+         // for each source file which has been optimised on a previous build, check if it is
+         // still present locally - i.e. whether it was part of latest commit. If so, restore
+         // the optimised versions of the file from the netlify cache
+         
          let srcFileWithFullPath='./public/images/'+srcFile;
-         // add here check to see if srcFile file still present (i.e is in lates commit)
          let found = await exists(srcFileWithFullPath);
          if (found){
             console.log(`${srcFileWithFullPath} found in image manifest and local files`);
          }
          else {
             console.log(`${srcFileWithFullPath} found in image manifest but not in local files`);
-
          }
          if (found){
             let files = sizes.map((s) => (
                './public/images/' + 
                path.dirname(srcFile) 
                + '/nextImageExportOptimizer/' 
-               + Path.parse(srcFile).name + 
+               + path.parse(srcFile).name + 
                '-opt-' + s + '.WEBP'
             ))
-            // console.log(files);
             
             for (let f of files){
                console.log('restoring ' +f + ' from netlify cache');
@@ -45,40 +47,19 @@ export const onPreBuild = async function ({ utils }) {
 
       }
 
-
-
    }
    else{
       console.log('Did not find image manifest.');
    }
-   
-   // let res3 = await utils.cache.restore('./public/images/test-export/karthik-sreenivas-rsx-joaKYrk-unsplash.jpg')
-   // console.log(res3);
-   // // ls('.next');
-
-   // let res4 = await utils.cache.restore('./public/images/test-export')
-   // console.log(res4);
-
-
 }
 
 // Cache file/directory for future builds.
 // Does not do anything if:
 //  - the file/directory does not exist locally
 export const onPostBuild = async function ({ utils }) {
-
    // Returns false if the file/directory does not exist. Returns true otherwise.
-   // let res2 = await utils.cache.save('./public/images/test-export/600x500.jpeg')
-   // console.log(res2);
-
-   // fs.appendFile('.next/images/test.txt', 'Hello content!', function (err) {
-   //    if (err) throw err;
-   //    console.log('Saved!');
-   //  });
-   // ls('.next');
-   let res4 = await utils.cache.save('./public/images')
-   console.log(res4);
-
+   let res = await utils.cache.save('./public/images')
+   console.log(res);
 }
 
 
@@ -90,20 +71,3 @@ async function exists (path) {
      return false
    }
 } 
-
-async function ls(dir){
-   // directory path
-
-   // list all files in the directory
-   fs.readdir(dir, (err, files) => {
-      if (err) {
-         throw err
-      }
-
-      // files object contains all files names
-      // log them on console
-      files.forEach(file => {
-         console.log(file)
-      })
-   })
-}
