3be73efd463bb90f4410d402744c9a6505eee56d
jay <michaeldjay@gmail.com>
2022-10-31 23:48:27 +0000

    add 2nd image to test page

--- pages/test.jsx
+++ pages/test.jsx
@@ -4,6 +4,8 @@ export default function Test(){
    return <>
       hi
       <ExportedImageFuture src="/images/test-export/600x500.jpeg" alt="puppy" width={600} height={500} useWebp={process.env.nextImageExportOptimizer_storePicturesInWEBP}/>
+      <ExportedImageFuture src="/test-lfs/karthik-sreenivas-rsx-joaKYrk-unsplash.jpg" sizes={'640px'} alt="autumn" width={4527} height={3015} style={{width: '50%', height: 'auto'}} priority useWebp={process.env.nextImageExportOptimizer_storePicturesInWEBP}/>
+
    </>
    
 }
