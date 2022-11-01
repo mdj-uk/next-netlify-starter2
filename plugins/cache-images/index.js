b0dd82cc6d9c8bfacb760e19287cd6c13539881d
jay <michaeldjay@gmail.com>
2022-11-01 11:24:52 +0000

    tidy up plugin and handle errors better

--- plugins/cache-images/index.js
+++ plugins/cache-images/index.js
@@ -1,73 +1,94 @@
 import fs from 'fs'
 import path from 'path'
 
+//  settings
+const defaultSizes = [10, 16, 32, 48, 64, 96, 128, 256, 384, 640, 750, 828, 1080, 1200, 1920, 2048, 3840];
+let sizes = [10, 32, 1000];
+let imageFolder = './public/images';
 
 export const onPreBuild = async function ({ utils }) {
-   
-   let imageManifestFile='./public/images/next-image-export-optimizer-hashes.json';
-   let sizes = [10, 32, 1000];
-   
-   let imageManifestRestored = await utils.cache.restore(imageManifestFile)
-   
-   if (imageManifestRestored){
-      console.log('Found image manifest. Will attempt to parse ... ');
-      
-      const fileContents = await fs.promises.readFile(imageManifestFile);
-      const data = JSON.parse(fileContents);
 
-      let preOptimisedImageFiles = Object.keys(data);
-      for (let srcFile of preOptimisedImageFiles){
-        
+   
+   // Restore manifest of source files which have been optimised on previous builds
+   let imageManifestFile = imageFolder + '/next-image-export-optimizer-hashes.json';
+   let imageManifestRestored = await utils.cache.restore(imageManifestFile)
+
+   if (imageManifestRestored) {
+      console.log('Found image manifest. Will attempt to parse ... ');
+
+      let preOptimisedImageFiles = [];
+      try {
+         const fileContents = await fs.promises.readFile(imageManifestFile);
+         const data = JSON.parse(fileContents);
+         preOptimisedImageFiles = Object.keys(data);
+      } catch (error) {
+         console.log('Error attempting to read ' + imageManifestFile);
+         console.error(error);         
+         return
+      }
+
+      for (let srcFile of preOptimisedImageFiles) {
+
          // for each source file which has been optimised on a previous build, check if it is
-         // still present locally - i.e. whether it was part of latest commit. If so, restore
+         //  present locally - i.e. whether it was part of latest commit. If so, restore
          // the optimised versions of the file from the netlify cache
-         
-         let srcFileWithFullPath='./public/images/'+srcFile;
+
+         let srcFileWithFullPath = path.join(imageFolder, srcFile);
          let found = await exists(srcFileWithFullPath);
-         if (found){
+         if (found) {
             console.log(`${srcFileWithFullPath} found in image manifest and local files`);
          }
          else {
             console.log(`${srcFileWithFullPath} found in image manifest but not in local files`);
          }
-         if (found){
+         if (found) {
             let files = sizes.map((s) => (
-               './public/images/' + 
-               path.dirname(srcFile) 
-               + '/nextImageExportOptimizer/' 
-               + path.parse(srcFile).name + 
+               imageFolder + '/' +
+               path.dirname(srcFile)
+               + '/nextImageExportOptimizer/'
+               + path.parse(srcFile).name +
                '-opt-' + s + '.WEBP'
             ))
-            
-            for (let f of files){
-               console.log('restoring ' +f + ' from netlify cache');
-               await utils.cache.restore(f)
+
+            for (let f of files) {
+               
+               try {
+                  await utils.cache.restore(f)
+                  console.log('restored ' + f + ' from netlify cache');
+               } catch (error) {
+                  console.log('Error restoring ' + f + ' from netlify cache');
+                  console.error(error);
+               }
             }
          }
 
       }
 
    }
-   else{
+   else {
       console.log('Did not find image manifest.');
    }
 }
 
-// Cache file/directory for future builds.
-// Does not do anything if:
-//  - the file/directory does not exist locally
 export const onPostBuild = async function ({ utils }) {
-   // Returns false if the file/directory does not exist. Returns true otherwise.
-   let res = await utils.cache.save('./public/images')
-   console.log(res);
+   // Cache file/directory for future builds.
+   // let imageFolder = './public/images'
+   try {
+      await utils.cache.save(imageFolder)
+      console.log(imageFolder + ' cached for future builds.');
+   } catch (error) {
+      console.log('Error attempting to cache ' + imageFolder + ' for future builds.');
+      console.error(error);
+   }
+
 }
 
 
-async function exists (path) {  
+async function exists(path) {
    try {
-     await fs.promises.access(path)
-     return true
+      await fs.promises.access(path)
+      return true
    } catch {
-     return false
+      return false
    }
 } 
